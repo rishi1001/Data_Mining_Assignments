70b1b64649f76b35a86a0a913b68a11399db4423
Prakhar Jagwani <prakharjagwani7@gmail.com>
2022-10-06 10:24:24 +0530

    update elbow_plot

--- HW2/Q3/elbow_plot.py
+++ HW2/Q3/elbow_plot.py
@@ -3,6 +3,9 @@ import numpy as np
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 
+def get_2_diff(inertia, i):
+    return ((inertia[i-1] - inertia[i]) - (inertia[i] - inertia[i+1]))/(inertia[i])
+
 if __name__ == "__main__":
     data = np.genfromtxt(sys.argv[1])
     print("Dataset: ", data.shape)
@@ -13,8 +16,15 @@ if __name__ == "__main__":
     for k in cluster_sizes:
         kmeans = KMeans(n_clusters=k,random_state=0).fit(data)
         inertia_vals.append(kmeans.inertia_)
+    
 
+    max_2_diff_index = 1
+    for k in range(1,len(inertia_vals)-1):
+        print(get_2_diff(inertia_vals, k))
+        if get_2_diff(inertia_vals, k) > get_2_diff(inertia_vals, max_2_diff_index):
+            max_2_diff_index = k        
     plt.plot(cluster_sizes, inertia_vals, marker='o')
+    plt.plot(cluster_sizes[max_2_diff_index], inertia_vals[max_2_diff_index], marker='o')
     plt.xticks(cluster_sizes)
     plt.ylim(ymin=0)
     plt.xlabel("Number of clusters")
