d8c1265fda460689538e77d2b01c57e7cde36129
Krishnanshu1907 <krishnanshu1907@gmail.com>
2022-10-30 17:43:29 +0530

    Debugged

--- Q1/main.py
+++ Q1/main.py
@@ -17,12 +17,20 @@ def read_data():
 
   df = pd.read_csv(path)
   df=df.drop(['Unnamed: 0'], axis=1)
-  for i in range(100):
+#   for i in range(len(df)-1):  # TODO : change this 
+#     # print("dddddddd")
+#     print(i)
+#     d=df.loc[i:i+1,:]
+#     d=d.reset_index(drop=True)
+#     dataset.append(data_point(d,G.mapping))
+
+  for i in range(2):  # TODO : CCHECk this 
     # print("dddddddd")
     print(i)
     d=df.loc[i:i+1,:]
     d=d.reset_index(drop=True)
     dataset.append(data_point(d,G.mapping))
+
 
   return dataset
 
@@ -46,6 +54,7 @@ print("yahi hu me")
 
 
 model = GCN(hidden_channels=16)
+model=model.double()
 optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
 criterion = torch.nn.MSELoss()          # TODO check this
 
@@ -59,16 +68,16 @@ def train(epoch):
     # batch wise training
     for data in dataset:
         optimizer.zero_grad()  # Clear gradients.
-        print(data.features)
+        #print(data.features)
     #   print(data.features.shape)
         out = model(data.features, G.edge_index, G.edge_weight).reshape(-1)   
     #   print(out.shape)
         loss = criterion(out, data.y[train_node_ids])
     #   make_dot(out).render("graph.dot", format="png")
     #   print(out)
-        print("dataaa y")
-        print(data.y)
-        print("out", out)
+        #print("dataaa y")
+        #print(data.y)
+        #print("out", out)
         loss.backward()
         optimizer.step()
         print(loss)
@@ -76,24 +85,24 @@ def train(epoch):
 
     print('epoch %d training loss: %.3f' % (epoch + 1, running_loss / (len(dataset))))
 
-def test(test_set,test=False):         # test=True for test set
+def test(test=False):         # test=True for test set
     model.eval()
     global best_loss
     global bestmodel
     running_loss = 0.0
     with torch.no_grad():
-        for data in test_set:
+        for data in dataset:
             out = model(data.features, G.edge_index, G.edge_weight).reshape(-1)
             if test:
-                loss = criterion(out, data.y[data.test_node_ids])
+                loss = criterion(out, data.y[test_node_ids])
             else:
-                loss = criterion(out, data.y[data.val_node_ids])
+                loss = criterion(out, data.y[val_node_ids])
             loss = criterion(out, data.y)
             running_loss += loss.item()
-            print(loss)
-            print("dataaa y")
-            print(data.y)
-            print("out", out)
+            #print(loss)
+            #print("dataaa y")
+            #print(data.y)
+            #print("out", out)
             print("test loss", loss)
             print("test acc", loss)
     
@@ -107,13 +116,14 @@ if __name__ == '__main__':
     print('Start Training')
     os.makedirs('./models', exist_ok=True)
 
-    num_epochs = 100
+    num_epochs = 1
     for epoch in range(num_epochs):  # loop over the dataset multiple times
         print('epoch ', epoch + 1)
         train(epoch)
-        test(val_node_ids)      # on validation set      
+        test()      # on validation set    
 
     print('performing test')
+    test(test=True)
     print('Finished Training')
 
     # Saving our trained model
