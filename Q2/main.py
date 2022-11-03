b066efd981ffaa7cd979c80d52841c3ae63cc7d1
Krishnanshu1907 <krishnanshu1907@gmail.com>
2022-11-04 01:12:33 +0530

    Added datasets

---
+++ Q2/main.py
@@ -0,0 +1,121 @@
+import torch
+import os
+from dataset import data_point, graph
+from model import STConv
+import pandas as pd
+import numpy as np
+from utils import *
+
+G=graph("../a3_datasets/d2_adj_mx.csv")
+print(G.edge_index)
+print(G.edge_weight.shape)
+
+
+def read_data():
+  dataset=[]
+  path='../a3_datasets/d2_X.csv'
+
+  df = pd.read_csv(path)
+  df=df.drop(['Unnamed: 0'], axis=1)
+#   for i in range(len(df)-1):  # TODO : change this 
+#     # print("dddddddd")
+#     print(i)
+#     d=df.loc[i:i+1,:]
+#     d=d.reset_index(drop=True)
+#     dataset.append(data_point(d,G.mapping))
+   
+  
+  
+
+# TODO masking for train and test while training & loss function
+# TODO add code for validation
+def convert(l,mapping):
+  m = {mapping[i]:i for i in range(len(mapping))}
+  return [m[str(i)] for i in l]  
+
+
+dataset = read_data()
+splits = np.load("../a3_datasets/d2_graph_splits.npz") 
+train_node_ids = convert(splits["train_node_ids"],G.mapping) 
+val_node_ids = convert(splits["val_node_ids"],G.mapping) 
+test_node_ids = convert(splits["test_node_ids"],G.mapping)
+
+# TODO ask if we can use featuers of test nodes also while training?
+
+
+model = STConv(hidden_channels=16)
+model=model.double()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
+criterion = torch.nn.MSELoss()
+
+best_loss = -1
+bestmodel = None
+
+def train(epoch):
+    model.train()
+
+    running_loss = 0.0
+    # batch wise training
+    for data in dataset:
+        optimizer.zero_grad()  # Clear gradients.
+        #print(data.features)
+    #   print(data.features.shape)
+        out = model(data.features, G.edge_index, G.edge_weight)  
+        loss = criterion(out[train_node_ids], data.y[train_node_ids])/len(train_node_ids)
+    #   make_dot(out).render("graph.dot", format="png")
+    #   print(out)
+        #print("dataaa y")
+        #print(data.y)
+        #print("out", out)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+
+    print('epoch %d training loss: %.3f' % (epoch + 1, running_loss / (len(dataset))))
+
+def test(test=False):         # test=True for test set
+    model.eval()
+    global best_loss
+    global bestmodel
+    running_loss = 0.0
+    with torch.no_grad():
+        for data in dataset:
+            out = model(data.features, G.edge_index, G.edge_weight)
+            if test:
+                loss = criterion(out[test_node_ids], data.y[test_node_ids])/len(test_node_ids)
+            else:
+                loss = criterion(out[val_node_ids], data.y[val_node_ids])/len(val_node_ids)
+            running_loss += loss.item()
+        print('epoch %d Test loss: %.3f' % (epoch + 1, running_loss / (len(dataset))))
+        
+    
+    if test==False and (best_loss==-1 or running_loss < best_loss):
+        best_loss=running_loss
+        # Saving our trained model
+        torch.save(model.state_dict(), './models/bestval.pth')
+        bestmodel = model
+
+if __name__ == '__main__':
+    print('Start Training')
+    os.makedirs('./models', exist_ok=True)
+
+    # TODO we can use scalar to fit transform the data, also pass that in evaluate metric
+
+
+    num_epochs = 1000
+    for epoch in range(num_epochs):  # loop over the dataset multiple times
+        # print('epoch ', epoch + 1)
+        train(epoch)
+        test()      # on validation set    
+
+    print('performing test')
+    test(test=True)
+    print('Finished Training')
+
+    MAE, MAPE, RMSE = evaluate_metric(bestmodel, dataset, G)
+    print("MAE: ", MAE)
+
+    # TODO use the plotting function from utils.py
+
+    # Saving our trained model
+    torch.save(model.state_dict(), './models/lastmodel.pth')
---
+++ Q2/utils.py
@@ -0,0 +1,49 @@
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+
+def evaluate_metric_scaler(model, data_iter, scaler):
+    model.eval()
+    with torch.no_grad():
+        mae, mape, mse = [], [], []
+        for x, y in data_iter:
+            y = scaler.inverse_transform(y.cpu().numpy()).reshape(-1)
+            y_pred = scaler.inverse_transform(
+                model(x).view(len(x), -1).cpu().numpy()
+            ).reshape(-1)
+            d = np.abs(y - y_pred)
+            mae += d.tolist()
+            mape += (d / y).tolist()
+            mse += (d**2).tolist()
+        MAE = np.array(mae).mean()
+        MAPE = np.array(mape).mean()
+        RMSE = np.sqrt(np.array(mse).mean())
+        return MAE, MAPE, RMSE
+
+
+def evaluate_metric(model, dataset, G):
+    model.eval()
+    with torch.no_grad():
+        mae, mape, mse = [], [], []
+        for data in dataset:
+            y = data.y
+            y_pred = model(data.features, G.edge_index, G.edge_weight)
+            d = np.abs(y - y_pred)
+            mae += d.tolist()
+            mape += (d / y).tolist()
+            mse += (d**2).tolist()
+        MAE = np.array(mae).mean()
+        MAPE = np.array(mape).mean()
+        RMSE = np.sqrt(np.array(mse).mean())
+        return MAE, MAPE, RMSE
+
+def plot(n, future, y, y_pred):
+    plt.figure(figsize=(12,6))
+    plt.title(f"Step {i+1}")
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.xticks(fontsize=20)
+    plt.yticks(fontsize=20)
+
+    plt.plot(np.arange(n), y, 'r', linewidth=2.0)
+    plt.plot(np.arange(n, n+future), y_pred, 'r:', linewidth=2.0)
