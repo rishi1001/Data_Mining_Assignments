7ac8207189965e8500f19f97c1fe3517bcc8c551
Krishnanshu1907 <krishnanshu1907@gmail.com>
2022-11-06 20:34:40 +0530

    Debugged model

--- Q1/dataset.py
+++ Q1/dataset.py
@@ -6,17 +6,13 @@ import pandas as pd
 import torch
 from torch_geometric.utils import dense_to_sparse
 # from torch.utils.data import Dataset
-from torch.utils.data import Dataset
-class graph():
-
-    def __init__ (self,path):
-        self.edge_index=None
-        self.edge_weight=None
-        self.mapping=None    ## index to node_id
-        self.read_graph(path)
-
-    def read_graph(self,path):
-        df = pd.read_csv(path,index_col=0)
+from torch_geometric.data import Data
+from torch_geometric.data import Dataset
+       
+class TimeSeries(Dataset):
+    def __init__(self,csv_file,graph_file,normalize) -> None:
+        super().__init__()
+        df = pd.read_csv(graph_file,index_col=0)
         cols=df.columns
         df.columns=[i for i in range(len(cols))]
         df=df.reset_index(drop=True)
@@ -24,72 +20,27 @@ class graph():
         t=torch.tensor(df.values)
         self.edge_index , self.edge_weight = dense_to_sparse(t)
         self.edge_weight=self.edge_weight
+        self.num_nodes = len(cols)
+        
+        if normalize:
+            self.edge_weight=self.edge_weight/torch.max(self.edge_weight)
+            self.edge_weight=self.edge_weight.double()
         #self.mapping ={i:cols[i] for i in range(len(cols))}                    
         self.mapping=cols
-
-class data_point():
-
-    def __init__(self,data,mapping):
-        self.features=None
-        self.y=None
-        self.num_features=1
-        self.generate_features(data,mapping)
-        self.generate_y(data,mapping)
-    
-    
-    def generate_features(self,data,mapping):
-        self.num_features=1
-        # TODO check do wee nedd [3] or [3,1]
-        l=[[data.iloc[0][mapping[i]]] for i in range(len(mapping))]
-        self.features=torch.tensor(l).double()
-
-    def generate_y(self,data,mapping):
-        # TODO check do wee nedd [3] or [3,1]
-        l=[[data.iloc[1][mapping[i]]] for i in range(len(mapping))]
-        self.y=torch.tensor(l).double()
         
-        
-    
-class TimeSeries(Dataset):
-    def __init__(self,csv_file,mapping) -> None:
-        super().__init__()
         df = pd.read_csv(csv_file)
         df=df.drop(['Unnamed: 0'], axis=1)
         i = np.arange(df.shape[0]-1)
         i_1 = i+1
         pairs = np.array([i,i_1]).T.ravel()
-        self.dataset = df.loc[pairs,mapping].to_numpy().reshape(df.shape[0]-1, 2, len(mapping),1)
+        self.dataset = df.loc[pairs,cols].to_numpy().reshape(df.shape[0]-1, 2, len(cols),1)
 
     def __len__(self):
         return len(self.dataset)
     
     def __getitem__(self, idx):
-        return {'x':torch.tensor(self.dataset[idx][0]).type(torch.DoubleTensor),'y': torch.tensor(self.dataset[idx][1]).type(torch.DoubleTensor)} 
-
-    
-# class TimeSeries(Dataset):
-#     def __init__(self,csv_file,graph_file) -> None:
-#         super().__init__()
-#         df = pd.read_csv(graph_file,index_col=0)
-#         cols=df.columns
-#         df.columns=[i for i in range(len(cols))]
-#         df=df.reset_index(drop=True)
-        
-#         t=torch.tensor(df.values)
-#         self.edge_index , self.edge_weight = dense_to_sparse(t)
-#         self.edge_weight=self.edge_weight
-#         #self.mapping ={i:cols[i] for i in range(len(cols))}                    
-#         self.mapping=cols
-        
-#         df = pd.read_csv(csv_file)
-#         df=df.drop(['Unnamed: 0'], axis=1)
-#         i = np.arange(df.shape[0]-1)
-#         i_1 = i+1
-#         pairs = np.array([i,i_1]).T.ravel()
-#         self.dataset = df.loc[pairs,cols].to_numpy().reshape(df.shape[0]-1, 2, len(cols),1)
-
-#     def __len__(self):
-#         return len(self.dataset)
-    
-#     def __getitem__(self, idx):
-#         return {'x':torch.tensor(self.dataset[idx][0]).type(torch.DoubleTensor),'y': torch.tensor(self.dataset[idx][1]).type(torch.DoubleTensor),'edge_weight':self.edge_weight,'edge_index':self.edge_index} 
+        # return {'x':torch.tensor(self.dataset[idx][0]).type(torch.DoubleTensor),'y': torch.tensor(self.dataset[idx][1]).type(torch.DoubleTensor),'edge_weight':self.edge_weight,'edge_index':self.edge_index} 
+        x=torch.tensor(self.dataset[idx][0]).type(torch.DoubleTensor)
+        y=torch.tensor(self.dataset[idx][1]).type(torch.DoubleTensor)
+        d= Data(x=x,edge_index=self.edge_index,edge_attr=self.edge_weight,y=y)
+        return d
--- Q1/main.py
+++ Q1/main.py
@@ -1,54 +1,52 @@
 import torch
 import os
-from dataset import  graph,TimeSeries
+from dataset import TimeSeries
 from model import GCN
 import pandas as pd
 import numpy as np
 from utils import *
-from torch.utils.data import DataLoader
+from torch_geometric.loader import DataLoader
 
-
-G=graph("../a3_datasets/d2_adj_mx.csv")
-print(G.edge_index)
-print(G.edge_weight.shape)
-
-# TODO masking for train and test while training & loss function
-# TODO add code for validation
 def convert(l,mapping):
   m = {mapping[i]:i for i in range(len(mapping))}
   return [m[str(i)] for i in l]  
 
+def get_train_node_ids(train_node_ids, batch_size):
+    train_ids = []
+    for i in range(batch_size):
+        for x in train_node_ids:
+            train_ids.append(x+i*dataset.num_nodes)
+    print(train_ids)
+    return train_ids
 
-dataset=TimeSeries("../a3_datasets/d2_X.csv",G.mapping)
-# print(dataset[0])
-# exit(0)
-splits = np.load("../a3_datasets/d2_graph_splits.npz") 
-train_node_ids = convert(splits["train_node_ids"],G.mapping) 
-val_node_ids = convert(splits["val_node_ids"],G.mapping) 
-test_node_ids = convert(splits["test_node_ids"],G.mapping)
-# train_node_ids = [0,1,2,3,4]
-# val_node_ids = [0,1,2,3,4] 
-# test_node_ids = [0,1,2,3,4]
-# # TODO ask if we can use featuers of test nodes also while training?
+### model-parameters
+batch_size=2
+hidden_layers=16
+lr=0.01
+weight_decay=5e-4
+num_epochs=100
+normalize=True
+
+dataset=TimeSeries("../a3_datasets/d1_X.csv","../a3_datasets/d1_adj_mx.csv",normalize)
+dataloader = DataLoader(dataset, batch_size=batch_size,shuffle=True, num_workers=0)
+
+splits = np.load("../a3_datasets/d1_graph_splits.npz") 
+train_node_ids = convert(splits["train_node_ids"],dataset.mapping) 
+val_node_ids = convert(splits["val_node_ids"],dataset.mapping) 
+test_node_ids = convert(splits["test_node_ids"],dataset.mapping)
+
+train_node_ids= get_train_node_ids(train_node_ids,2)
+val_node_ids= get_train_node_ids(val_node_ids,2)
 
 
-dataloader = DataLoader(dataset, batch_size=1,shuffle=True, num_workers=0)
 
-model = GCN(hidden_channels=16)
+model = GCN(hidden_channels=hidden_layers)
 model=model.double()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
+optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
 criterion = torch.nn.MSELoss()
 
 best_loss = -1
 bestmodel = None
-
-# for i,data in enumerate(dataloader):
-#     print(i)
-#     print(data['x'].shape)
-#     print(data['x'])
-#     print(X)
-#     exit(0)
-# exit(0)
 
 def train(epoch):
     model.train()
@@ -58,17 +56,17 @@ def train(epoch):
     for i,data in enumerate(dataloader):
         optimizer.zero_grad()  # Clear gradients.
         #print(data.features)
-        ss=data['x'].shape[1]
-        X=data['x'].reshape(ss,1)
-        Y=data['y'].reshape(ss,1)
-        out = model(X, G.edge_index,G.edge_weight)  
-        loss = criterion(out[train_node_ids], Y[train_node_ids])/len(train_node_ids)
+        out = model(data.x, data.edge_index,data.edge_weight)  
+        # print(out)
+        loss = criterion(out[train_node_ids], data.y[train_node_ids])
+        # print(loss)
+
 
         loss.backward()
         optimizer.step()
         running_loss += loss.item()
 
-    print('epoch %d training loss: %.3f' % (epoch + 1, running_loss / (len(dataset))))
+    print('epoch %d training loss: %.3f' % (epoch + 1, running_loss / batch_size))
 
 def test(test=False):         # test=True for test set
     model.eval()
@@ -77,16 +75,13 @@ def test(test=False):         # test=True for test set
     running_loss = 0.0
     with torch.no_grad():
         for data in dataloader:
-            ss=data['x'].shape[1]
-            X=data['x'].reshape(ss,1)
-            Y=data['y'].reshape(ss,1)
-            out = model(X, G.edge_index, G.edge_weight)
+            out = model(data.x, data.edge_index, data.edge_weight)
             if test:
-                loss = criterion(out[test_node_ids], Y[test_node_ids])/len(test_node_ids)
+                loss = criterion(out[test_node_ids], data.y[test_node_ids])
             else:
-                loss = criterion(out[val_node_ids], Y[val_node_ids])/len(val_node_ids)
+                loss = criterion(out[val_node_ids], data.y[val_node_ids])
             running_loss += loss.item()
-        print('epoch %d Test loss: %.3f' % (epoch + 1, running_loss / (len(dataset))))
+        print('epoch %d Test loss: %.3f' % (epoch + 1, running_loss / batch_size))
         
     
     if test==False and (best_loss==-1 or running_loss < best_loss):
@@ -101,10 +96,7 @@ if __name__ == '__main__':
 
     # TODO we can use scalar to fit transform the data, also pass that in evaluate metric
 
-
-    num_epochs = 100
-    for epoch in range(num_epochs):  # loop over the dataset multiple times
-        # print('epoch ', epoch + 1)
+    for epoch in range(num_epochs): 
         train(epoch)
         test()      # on validation set    
 
