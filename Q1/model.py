03ce41f4b4ea06b37cf606b97935b43f9abbb209
Krishnanshu1907 <krishnanshu1907@gmail.com>
2022-11-12 21:10:31 +0530

    Remove batch_norm layer

--- Q1/model.py
+++ Q1/model.py
@@ -8,9 +8,9 @@ class GCN(torch.nn.Module):
     def __init__(self, hidden_channels):
         super().__init__()
         torch.manual_seed(1234567)
-        self.batch_norm0 = torch.nn.BatchNorm1d(1)
+        self.batch_norm0 = torch.nn.BatchNorm1d(1,track_running_stats=False,affine=False)
         self.conv1 = GCNConv(1, hidden_channels)            # input features = 1
-        self.batch_norm1 = torch.nn.BatchNorm1d(hidden_channels)
+        self.batch_norm1 = torch.nn.BatchNorm1d(hidden_channels,track_running_stats=False,affine=False)
         self.conv2 = GCNConv(hidden_channels, 2*hidden_channels)
         # mlp
         self.lin1 = Linear(2*hidden_channels, hidden_channels)
@@ -18,11 +18,11 @@ class GCN(torch.nn.Module):
 
     def forward(self, x, edge_index,edge_weight):       # TODO add batchnorm
         # print(x.shape)
-        # print(x)
-        x = self.batch_norm0(x)
+        # # print(x)
+        # x = self.batch_norm1(x)
         # print(x)
         x = self.conv1(x, edge_index,edge_weight)
-        x = self.batch_norm1(x)
+        # x = self.batch_norm1(x)
         x = x.relu()
         # x = F.dropout(x, p=0.5, training=self.training)
         x = self.conv2(x, edge_index,edge_weight)           # node embeddings
